6c6721453cf58d22b90c9011f298794ba3fc5166
muhammad <froma2zi@gmail.com>
2019-03-31 00:37:20 +0200

    add new route to handle vue pages to solve refresh issue

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/{vue_capture?}', function () {
+    return view('home');
+})->where('vue_capture', '[\/\w\.-]*');
